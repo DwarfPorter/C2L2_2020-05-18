8b9f5265414fc58cf346ea7b8b703e98580ff84a
DwarfPorter <mrvlmor@gmail.com>
2020-05-18 21:26:22 +0300

    Диалог с 3 кнопками

--- alertdialog/app/src/main/java/ru/geekbrains/alertdialog/MainActivity.java
+++ alertdialog/app/src/main/java/ru/geekbrains/alertdialog/MainActivity.java
@@ -30,6 +30,37 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Диалог открыт", Toast.LENGTH_SHORT).show();
         }
     };
+    private View.OnClickListener clickAlertDialog3 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(R.string.exclamation)
+                    .setMessage("2 * 2 = 4?")
+                    .setCancelable(true)
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "Нет!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNeutralButton(R.string.dunno, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "Не знаю!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "Да!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +68,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.alertDialog1).setOnClickListener(clickAlertDialog1);
+        findViewById(R.id.alertDialog3).setOnClickListener(clickAlertDialog3);
     }
 }
